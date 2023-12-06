6287e79743f5e9b36c997a3e3ed69f7c4a1ec909
Rachele <rachele.trimbo@gmail.com>
2023-12-06 16:20:41 +0100

    first commit

---
+++ task.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("inserTask");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
+
+form.addEventListener("submit", function () {
+  const task = document.getElementById("newTask");
+
+  const list = document.createElement("ul");
+  list.innerHTML = `<ul>
+  <li>${task.value}</li><button></button>
+  </ul>
+  `;
+
+  const listSection = document.getElementById("listaTask");
+  listSection.appendChild(list);
+});
