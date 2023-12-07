4c576348c333d93030949af23d08777c5a801b01
Rachele <rachele.trimbo@gmail.com>
2023-12-07 08:57:00 +0100

    completed

--- task.js
+++ task.js
@@ -1,26 +1,22 @@
 const form = document.getElementById("inserTask");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-});
 
-form.addEventListener("submit", function () {
   const task = document.getElementById("newTask");
   const list = document.createElement("ul");
   list.innerHTML = `<ul>
-  <li id="item" class>${task.value}</li>
-  <button id="delete">Delete</button>
-  
+  <li class="lista">${task.value}<button class="button">Delete</button></li>
   </ul>
   `;
 
   const listSection = document.getElementById("listaTask");
   listSection.appendChild(list);
-
-  const li = document.getElementById("item");
-  li.addEventListener("click", function () {
-    li.style.textDecoration = "line-through";
-  });
-
-
-
-
+  // const sbarrato = function () {
+  const li = document.getElementsByClassName("lista");
+  for (let i = 0; i < li.length; i++) {
+    li[i].addEventListener("click", function (e) {
+      li[i].style.textDecoration = "line-through";
+    });
+  }
+  // };
+});
